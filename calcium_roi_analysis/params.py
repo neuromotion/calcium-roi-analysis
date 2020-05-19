2adf037870ecf50c5e292b75a6fa1974658a81fb
Isabel Restrepo <isabelrestre@gmail.com>
2020-05-19 18:00:46 -0400

    feat: "Finish" sample find_roi notebook

--- calcium_roi_analysis/params.py
+++ calcium_roi_analysis/params.py
@@ -6,11 +6,7 @@ def prepare_paths(param_file):
     with open(param_file, "r") as file:
         params = yaml.load(file, Loader=yaml.FullLoader)
 
-    data_base_path = params["data_base_path"]
-    output_base_path = params["output_base_path"]
-    data_name = params["data_name"]
+    data_path = params["input_mip"]
+    output_path = params["output_base_path"]
         
-    data_path = data_base_path+ "/" + data_name
-    output_path = output_base_path + "/" + data_name
-
     return params, data_path, output_path
